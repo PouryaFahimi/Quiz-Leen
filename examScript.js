2ebbde063c84a667b86420907e836eb423110411
PouryaFahimi <p.f.challenger369@gmail.com>
2024-08-07 13:13:21 +0330

    updated and matched

--- examScript.js
+++ examScript.js
@@ -146,6 +146,7 @@ async function receiveQuiz() {
     })
     .catch((error) => {
       console.error("Error:", error);
+      // here must put a error window
     });
 
   // const response = await fetch(apiUrl);
